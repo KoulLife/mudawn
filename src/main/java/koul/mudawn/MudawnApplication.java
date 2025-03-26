8dca5bba919e5df00221b6870f86a8c2381365af
Koul <dongik0122@gmail.com>
2025-03-27 00:05:54 +0900

    feat: add JPA configuration for MariaDB

---
+++ src/main/java/koul/mudawn/MudawnApplication.java
@@ -0,0 +1,13 @@
+package koul.mudawn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MudawnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MudawnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/koul/mudawn/MudawnApplicationTests.java
@@ -0,0 +1,13 @@
+package koul.mudawn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MudawnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
